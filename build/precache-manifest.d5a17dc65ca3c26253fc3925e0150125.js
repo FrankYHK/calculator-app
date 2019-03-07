31115e3662dc09f1c4535e5356a6af03805d0756
Frank You <frankyou@FrankdeMacBook-Pro.local>
2019-03-07 17:26:17 +0800

    update

---
+++ build/precache-manifest.d5a17dc65ca3c26253fc3925e0150125.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "3739c0970fc42a925465",
+    "url": "/static/js/main.59b9b890.chunk.js"
+  },
+  {
+    "revision": "3791395ae6897930f09c",
+    "url": "/static/js/2.b19c2b74.chunk.js"
+  },
+  {
+    "revision": "3739c0970fc42a925465",
+    "url": "/static/css/main.14ce759d.chunk.css"
+  },
+  {
+    "revision": "03910a7665cf7362b71257f9f7943048",
+    "url": "/index.html"
+  }
+];
